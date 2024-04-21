6a3e9658e690ea7d4c8e2f9d061322155479462d
izmi.komar <komar.izmi@gmail.com>
2024-04-21 17:02:48 +0700

    Add excludeFields utility function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,13 @@ export const generateId = (length = 21) => {
     '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   return customAlphabet(alphabets, length)();
 };
+
+export const excludeFields = <T>(object: T, keys: Array<keyof T>): T => {
+  if (!Object.keys(object).length) return object;
+
+  const result = { ...object };
+  keys.forEach((key) => {
+    delete result[key];
+  });
+  return result;
+};
