4268817ce257bb6f541975552d82ba1b458e936f
izmi.komar <komar.izmi@gmail.com>
2024-04-22 13:52:09 +0700

    Refactor app.controller.spec.ts and app.service.ts, and add caching functionality

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { Response } from 'express';
+import { ResponseData } from './lib/utils';
+import { CACHE_MANAGER, CacheModule } from '@nestjs/cache-manager';
+import { PrismaService } from './service/prisma-service';
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register()],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+          },
+        },
+        PrismaService,
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('getHello', () => {
+    it('should return the response from the AppService', async () => {
+      const result: ResponseData = {
+        statusCode: 200,
+        statusMessage: 'OKs',
+        success: true,
+      };
+      jest
+        .spyOn(appService, 'getHelloWithCaching')
+        .mockImplementation(() => Promise.resolve(result));
+
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      await appController.getHello(res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(result.statusCode);
+      expect(res.send).toHaveBeenCalledWith(result);
     });
   });
 });
