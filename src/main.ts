e3aed823f302b8871bb6deca8d350eefdef58d46
izmi.komar <komar.izmi@gmail.com>
2024-04-22 21:07:22 +0700

    Update API documentation and add contact and license information

--- src/main.ts
+++ src/main.ts
@@ -25,14 +25,29 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const config = new DocumentBuilder()
     .addBearerAuth()
-    .setTitle('NestJS API')
+    .setTitle('NestJS Starter API')
     .setDescription('The NestJS API description')
     .setVersion('1.0')
+    .setContact(
+      'izmikomar',
+      'https://github.com/komarapc',
+      'komar.izmi@gmail.com',
+    )
+    .setLicense('MIT', 'https://opensource.org/licenses/MIT')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document, {
     swaggerOptions: {
       defaultModelsExpandDepth: -1,
+      // docExpansion: 'none',
+      filter: true,
+      showRequestDuration: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+      syntaxHighlight: {
+        activate: true,
+        theme: 'arta',
+      },
     },
     customCss: `.swagger-ui .topbar { display: none }`,
     customCssUrl: '../public/css/theme-openapi.css',
